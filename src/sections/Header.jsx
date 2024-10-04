cf26787f96e769e8172819255ecb68e9db2317b1
Jaspreet <jaspreetkn92@gmail.com>
2024-10-04 10:46:01 +0100

    added conditional styling using clsx for the header

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { useState } from 'react';
 import { Link as LinkScroll } from 'react-scroll';
 
@@ -21,7 +22,12 @@ const Header = () => {
           <img src="/images/xora.svg" alt="logo" width={115} height={55} />
         </a>
 
-        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+        <div
+          className={clsx(
+            'w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0',
+            isOpen ? 'max-lg:opacity-100' : 'max-lg:pointer-events-none'
+          )}
+        >
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
