a3c6b55ad30f217ed6b3283513b6104a82f4bb36
Jaspreet <jaspreetkn92@gmail.com>
2024-10-04 10:51:02 +0100

    added background images to the mobile header

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -39,7 +39,15 @@ const Header = () => {
                 </li>
 
                 <li className="nav-logo">
-                  <LinkScroll>
+                  <LinkScroll
+                    to="hero"
+                    offset={100}
+                    spy
+                    smooth
+                    className={clsx(
+                      'max-lg:hidden transition-transform duration-500 cursor-pointer'
+                    )}
+                  >
                     <img
                       src="/images/xora.svg"
                       width={160}
@@ -56,6 +64,23 @@ const Header = () => {
                 </li>
               </ul>
             </nav>
+
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+              <img
+                src="/images/bg-outlines.svg"
+                width={960}
+                height={380}
+                alt="outline"
+                className="relative z-2"
+              />
+              <img
+                src="/images/bg-outlines-fill.png"
+                width={960}
+                height={380}
+                alt="outline"
+                className="absolute inset-0 mix-blend-soft-light opacity-5"
+              />
+            </div>
           </div>
         </div>
 
