33a45b677ae7dc4ee0115a69c3574a121fdcab2e
Jaspreet <jaspreetkn92@gmail.com>
2024-10-04 10:31:48 +0100

    added responsive styling and the hamburger button

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,6 +1,10 @@
 import { Link as LinkScroll } from 'react-scroll';
 
-const NavLink = ({ title }) => <LinkScroll>{title}</LinkScroll>;
+const NavLink = ({ title }) => (
+  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
+    {title}
+  </LinkScroll>
+);
 
 const Header = () => {
   return (
@@ -10,35 +14,41 @@ const Header = () => {
           <img src="/images/xora.svg" alt="logo" width={115} height={55} />
         </a>
 
-        <div className="w-full border-2 border-amber-400">
-          <nav>
-            <ul className="flex max-lg:block max-lg:px-12">
-              <li className="nav-li">
-                <NavLink title="features" />
-                <div className="dot" />
-                <NavLink title="pricing" />
-                <div className="dot" />
-              </li>
+        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+          <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+            <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
+              <ul className="flex max-lg:block max-lg:px-12">
+                <li className="nav-li">
+                  <NavLink title="features" />
+                  <div className="dot" />
+                  <NavLink title="pricing" />
+                  <div className="dot" />
+                </li>
 
-              <li className="nav-logo">
-                <LinkScroll>
-                  <img
-                    src="/images/xora.svg"
-                    width={160}
-                    height={55}
-                    alt="logo"
-                  />
-                </LinkScroll>
-              </li>
+                <li className="nav-logo">
+                  <LinkScroll>
+                    <img
+                      src="/images/xora.svg"
+                      width={160}
+                      height={55}
+                      alt="logo"
+                    />
+                  </LinkScroll>
+                </li>
 
-              <li className="nav-li">
-                <NavLink title="faq" />
-                <div className="dot" />
-                <NavLink title="download" />
-              </li>
-            </ul>
-          </nav>
+                <li className="nav-li">
+                  <NavLink title="faq" />
+                  <div className="dot" />
+                  <NavLink title="download" />
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
+
+        <button className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center">
+          <img src="/images/magic.svg" alt="hamburger" />
+        </button>
       </div>
     </header>
   );
