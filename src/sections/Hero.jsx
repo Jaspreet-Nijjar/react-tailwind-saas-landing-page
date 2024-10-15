780c1c65b375628e34b483a0002d60dddaeb8f3f
Jaspreet <jaspreetkn92@gmail.com>
2024-10-15 11:48:35 +0100

    added LinkScroll

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,4 @@
-import { Element } from 'react-scroll';
+import { Element, Link as LinkScroll } from 'react-scroll';
 
 const Hero = () => {
   return (
@@ -12,6 +12,13 @@ const Hero = () => {
             <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
               Amazingly Simple
             </h1>
+            <p className="max-w-440 mb-14 body-1 max-md:mb-10">
+              We designed XORA AI Video Editor to be an easy to use, quick to
+              learn and surprisingly powerful
+            </p>
+            <LinkScroll to="features" offset={-100} spy smooth>
+              TEXT
+            </LinkScroll>
           </div>
         </div>
       </Element>
