d2bec1cba77c678e05436ba0a65227d255bc617b
Jaspreet <jaspreetkn92@gmail.com>
2024-10-15 11:42:11 +0100

    creating the hero component

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,17 @@
+import { Element } from 'react-scroll';
+
+const Hero = () => {
+  return (
+    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 border-2 border-amber-400">
+      <Element name="hero">
+        <div className="container">
+          <div className="relative z-2 max-w-512 max-lg:max-w-388">
+            <div>Video Editing</div>
+          </div>
+        </div>
+      </Element>
+    </section>
+  );
+};
+
+export default Hero;
